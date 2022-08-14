c5ec43e8f01ce537fc4ca5dce929b1a5549a769d
Gerardo Arturo Miranda Abaid <gerardoarturomirandagodoy@gmail.com>
2022-08-14 14:39:57 -0500

    [ADD]BookDetail Component Finished

--- src/app/views/book-detail/book-detail.component.ts
+++ src/app/views/book-detail/book-detail.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from 'src/app/models/Book';
 import { BookDetailService } from './book-detail.service';
-import { faAngleLeft, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faWindowMinimize, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { ModalService } from 'src/app/components/modal/modal.service';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { Icon } from '@fortawesome/fontawesome-svg-core';
 @Component({
   selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
@@ -11,7 +15,10 @@ import { ModalService } from 'src/app/components/modal/modal.service';
 })
 export class BookDetailComponent implements OnInit {
   book!: Book
-  faAngleLeft!: IconDefinition 
+  faWindowMinimize!: IconDefinition 
+  faGoogle! : IconDefinition
+  faPlus!: IconDefinition;
+  faStar!: IconDefinition
   constructor(private activatedRoute: ActivatedRoute, private router: Router, protected bookDetailService: BookDetailService, private modal: ModalService) {
     this.instantiateVariables()
     this.activatedRoute.params.subscribe(async routeParams => {
@@ -25,19 +32,37 @@ export class BookDetailComponent implements OnInit {
 
   instantiateVariables(){
     this.book = new Book()
-    this.faAngleLeft = faAngleLeft
+    this.faWindowMinimize = faWindowMinimize
+    this.faGoogle = faGoogle
+    this.faPlus = faPlus
+    this.faStar = faStar
   }
 
 
   private fetchBook(bookId: string = ""){
     this.bookDetailService.fetchBook(bookId).subscribe((book)=> {
       this.book = book
-      this.book.volumeInfo.title
+      this.manageNullBookValues()
     })
   }
   
   back(){
     this.modal.closeModal()
     this.router.navigateByUrl("")
+  }
+
+  manageNullBookValues(){
+    this.book.volumeInfo.description == null ? this.book.volumeInfo.description = "No summary available for this title." : null
+    this.book.volumeInfo.authors == [] ? this.book.volumeInfo.authors = ["No authors available for this title"] : null
+    this.book.volumeInfo.publisher == null ? this.book.volumeInfo.publisher = "No publishers available for this title" : null
+    this.book.volumeInfo.averageRating == null ? this.book.volumeInfo.averageRating = 1  : this.book.volumeInfo.averageRating = Math.ceil(this.book.volumeInfo.averageRating)
+  }
+
+  openWindow(){
+    window.open(this.book.volumeInfo.previewLink, "_blank");
+  }
+
+  counter(i: number) {
+    return new Array(i);
   }
 }
